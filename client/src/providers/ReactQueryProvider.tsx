0ce14ced0e4767215f4ac61b2765bb84ffa0daf6
ABHAY-100 <abhaybalakrishnan884@gmail.com>
2025-08-04 18:21:33 +0530

    config client

---
+++ client/src/providers/ReactQueryProvider.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
+import { PropsWithChildren } from "react";
+
+export default function ReactQueryProvider({ children }: PropsWithChildren) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 3 * 60 * 1000,
+            gcTime: 5 * 60 * 1000,
+            retry: 2
+          },
+        },
+      })
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+}
