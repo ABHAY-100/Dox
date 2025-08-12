a8c52e6d21a58be6f30cec462696d23ee06f738c
sreyas <sreyas1045@gmail.com>
2025-08-12 20:27:11 +0530

    init express

---
+++ server/js/server.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
